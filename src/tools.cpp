cf78de9a717825f8885312720a94fe6dcd14864f
Keith Chester <kchester@gmail.com>
2019-02-12 19:11:58 -0800

    feature: Calculate the Jacobian

--- src/tools.cpp
+++ src/tools.cpp
@@ -43,8 +43,32 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+  MatrixXd Hj(3, 4);
+
+   //Recover the state parameters
+   float px = x_state(0);
+   float py = x_state(1);
+   float vx = x_state(2);
+   float vy = x_state(3);
+
+   //Error out if division by zero
+   if(px == 0 && py == 0){
+      cout << "Error - both px and py are 0, thus creating a divide by zero scenario when calculating the Jacobian";
+   }
+
+   //Calculate commonly used sections
+   float vxpy = vx * py;
+   float vypx = vy * px;
+   float px_2 = px * px;
+   float py_2 = py * py;
+   float denom_2  = px_2 + py_2;
+   float denom_sqrt = sqrt(denom_2);
+   float vypx_vxpy = vypx - vxpy;
+   float denom_32 = pow(denom_sqrt, 3/2);
+
+   Hj << px / denom_sqrt, py / denom_sqrt, 0, 0,
+      -1 * py / denom_2, px / denom_2, 0, 0,
+      (py * vypx_vxpy)/denom_32, (px*vypx_vxpy)/denom_32, px/denom_sqrt, py/denom_sqrt; 
+
+   return Hj;
 }
